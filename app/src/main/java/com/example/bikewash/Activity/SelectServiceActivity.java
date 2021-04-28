08f09145390514cb1bd3da240a3f96d0833dea10
Deepak <deepak@dumpin.in>
2021-04-28 07:58:54 +0530

    SelectServiceActivity design and java code done. Except firebase(text upload) code

--- app/src/main/java/com/example/bikewash/Activity/SelectServiceActivity.java
+++ app/src/main/java/com/example/bikewash/Activity/SelectServiceActivity.java
@@ -1,14 +1,33 @@
 package com.example.bikewash.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
 
-import com.example.bikewash.Adapter.SelectServiceAdapter;
 import com.example.bikewash.R;
-public class SelectServiceActivity extends AppCompatActivity {
+import com.example.bikewash.Utility.BaseActivity;
 
-    private RecyclerView uploadImagesOfVehicle;
+import static com.example.bikewash.Utility.Constants.AUTO_SERVICE;
+import static com.example.bikewash.Utility.Constants.BIKE_SERVICE;
+import static com.example.bikewash.Utility.Constants.CAR_SERVICE;
+import static com.example.bikewash.Utility.Constants.OTHER_SERVICE;
+import static com.example.bikewash.Utility.Constants.TEMPO_SERVICE;
+import static com.example.bikewash.Utility.Constants.TRACTOR_SERVICE;
+import static com.example.bikewash.Utility.Constants.TRUCK_SERVICE;
+public class SelectServiceActivity extends BaseActivity implements View.OnClickListener {
+
+    private CardView bikeCard, carCard, tempoCard, tractorCard, truckCard, autoCard, otherCard;
+    private CheckBox bikeCheckBox, carCheckBox, tempoCheckBox, tractorCheckBox, truckCheckBox,
+            autoCheckBox, otherCheckBox;
+    private EditText timeToReach, vehicleName;
+    private Button submitButton;
+    int serviceSelectedIs = BIKE_SERVICE;
+    private String vehicleNameData;
+//    private static final String TAG = "SelectServiceActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +37,102 @@ public class SelectServiceActivity extends AppCompatActivity {
     }
 
     private void findView() {
-        
+        bikeCard = findViewById( R.id.bikeCard );
+        bikeCheckBox = findViewById( R.id.bikeCheckBox );
+        carCard = findViewById( R.id.carCard );
+        carCheckBox = findViewById( R.id.carCheckBox );
+        tempoCard = findViewById( R.id.tempoCard );
+        tempoCheckBox = findViewById( R.id.tempoCheckBox );
+        tractorCard = findViewById( R.id.tractorCard );
+        tractorCheckBox = findViewById( R.id.tractorCheckBox );
+        truckCard = findViewById( R.id.truckCard );
+        truckCheckBox = findViewById( R.id.truckCheckBox );
+        autoCard = findViewById( R.id.autoCard );
+        autoCheckBox = findViewById( R.id.autoCheckBox );
+        otherCard = findViewById( R.id.otherCard );
+        otherCheckBox = findViewById( R.id.otherCheckBox );
+        vehicleName = findViewById( R.id.vehicleName );
+        timeToReach = findViewById( R.id.timeToReach );
+        submitButton = findViewById( R.id.submitButton );
+        bikeCard.setOnClickListener( this );
+        carCard.setOnClickListener( this );
+        tempoCard.setOnClickListener( this );
+        truckCard.setOnClickListener( this );
+        tractorCard.setOnClickListener( this );
+        autoCard.setOnClickListener( this );
+        otherCard.setOnClickListener( this );
+        submitButton.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == bikeCard) {
+            checkBoxChecked( true, false, false, false, false, false, false );
+            serviceSelectedIs = BIKE_SERVICE;
+        } else if (v == carCard) {
+            checkBoxChecked( false, true, false, false, false, false, false );
+            serviceSelectedIs = CAR_SERVICE;
+            vehicleName.setVisibility( View.GONE );
+        } else if (v == tempoCard) {
+            checkBoxChecked( false, false, true, false, false, false, false );
+            serviceSelectedIs = TEMPO_SERVICE;
+            vehicleName.setVisibility( View.GONE );
+        } else if (v == truckCard) {
+            checkBoxChecked( false, false, false, true, false, false, false );
+            serviceSelectedIs = TRUCK_SERVICE;
+            vehicleName.setVisibility( View.GONE );
+        } else if (v == tractorCard) {
+            checkBoxChecked( false, false, false, false, true, false, false );
+            serviceSelectedIs = TRACTOR_SERVICE;
+            vehicleName.setVisibility( View.GONE );
+        } else if (v == autoCard) {
+            checkBoxChecked( false, false, false, false, false, true, false );
+            serviceSelectedIs = AUTO_SERVICE;
+            vehicleName.setVisibility( View.GONE );
+        } else if (v == otherCard) {
+            checkBoxChecked( false, false, false, false, false, false, true );
+            serviceSelectedIs = OTHER_SERVICE;
+            vehicleName.setVisibility( View.VISIBLE );
+        } else if (v == submitButton) {
+            String reachingTime = timeToReach.getText().toString();
+            vehicleNameData = vehicleName.getText().toString();
+            if (serviceSelectedIs == OTHER_SERVICE) {
+                if (vehicleNameData.equalsIgnoreCase( "" )) {
+                    vehicleName.setError( "Please provide vehicle name" );
+                } else {
+                    checkReachTimeAndCard( reachingTime );
+                }
+            } else {
+                checkReachTimeAndCard( reachingTime );
+            }
+        }
+    }
+
+    private void checkBoxChecked(boolean bike, boolean car, boolean tempo, boolean truck, boolean tractor, boolean auto, boolean other) {
+        bikeCheckBox.setChecked( bike );
+        carCheckBox.setChecked( car );
+        tempoCheckBox.setChecked( tempo );
+        truckCheckBox.setChecked( truck );
+        tractorCheckBox.setChecked( tractor );
+        autoCheckBox.setChecked( auto );
+        otherCheckBox.setChecked( other );
+    }
+
+    private void checkReachTimeAndCard(String reachingTime) {
+        if (reachingTime.equalsIgnoreCase( "" )) {
+            timeToReach.setError( "Please provide time to reach at service station" );
+        } else {
+            Toast.makeText( this, String.valueOf( serviceSelectedIs ), Toast.LENGTH_SHORT ).show();
+            sendDataToFireBase( serviceSelectedIs, reachingTime );
+        }
+    }
+
+    private void sendDataToFireBase(int serviceSelectedIs, String reachTime) {
+        String vehicle_Name;
+        if (serviceSelectedIs == OTHER_SERVICE) {
+            vehicle_Name = vehicleNameData;
+        } else {
+            vehicle_Name = "";
+        }
     }
 }
--- app/src/main/java/com/example/bikewash/Utility/Constants.java
+++ app/src/main/java/com/example/bikewash/Utility/Constants.java
@@ -1,4 +1,5 @@
 package com.example.bikewash.Utility;
+import com.example.bikewash.Activity.SelectServiceActivity;
 public class Constants {
 
     //OnActivityResult/Camera
@@ -9,7 +10,7 @@ public class Constants {
     public static final int UPDATE_PROFILE_CODE = 103;
     public static final int FROM_SIGN_UP = 104;
     public static final int RC_SIGN_IN = 105;
-    
+
     //SharePref
     public static final String AUTH_TOKEN = "authToken";
     public static final String BEARER = "Bearer ";
@@ -21,4 +22,13 @@ public class Constants {
     public static final String OUT_TIME = "outTime";
     public static final String OUT_AM_PM = "outAmPm";
     public static final String OUT_DATE = "outDate";
+
+    //SelectServiceActivity
+    public static final int BIKE_SERVICE = 1;
+    public static final int CAR_SERVICE = 2;
+    public static final int TEMPO_SERVICE = 3;
+    public static final int TRACTOR_SERVICE = 4;
+    public static final int TRUCK_SERVICE = 5;
+    public static final int AUTO_SERVICE = 6;
+    public static final int OTHER_SERVICE = 7;
 }
